a557f2405a6563baef36d179810b21cd6e5a502d
ohta <yukiohta234@gmail.com>
2016-06-30 23:26:41 +0900

    add documents

--- ch02/ex01-tempconv/conv.go
+++ ch02/ex01-tempconv/conv.go
@@ -1,25 +1,31 @@
 package tempconv
 
+// CToF converts Celsius to Fahrenheit.
 func CToF(c Celsius) Fahrenheit {
 	return Fahrenheit(c*9/5 + 32)
 }
 
+// FToC converts Fahrenheit to Celsius.
 func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
 }
 
+// KToF converts Kelvin to Fahrenheit.
 func KToF(k Kelvin) Fahrenheit {
 	return CToF(KToC(k))
 }
 
+// KToC converts Kelvin to Celsius.
 func KToC(k Kelvin) Celsius {
 	return Celsius(k - 273.15)
 }
 
+// CToK converts Celsius to Kelvin.
 func CToK(c Celsius) Kelvin {
 	return Kelvin(c + 273.15)
 }
 
+// FToK converts Fahrenheit to Kelvin.
 func FToK(f Fahrenheit) Kelvin {
 	return CToK(FToC(f))
 }
--- ch02/ex01-tempconv/tempconv.go
+++ ch02/ex01-tempconv/tempconv.go
@@ -4,24 +4,37 @@ import (
 	"fmt"
 )
 
+// Celsius represents temparature in the degree Celcius (°C).
 type Celsius float64
+
+// Fahrenheit represents temparature in the degree Fahrenheit (°F).
 type Fahrenheit float64
+
+// Kelvin represents temparature in the degree Kelvin (K).
 type Kelvin float64
 
 const (
+	// AbsoluteZeroC represents value for the absolute zero temparature.
 	AbsoluteZeroC Celsius = -273.15
-	FreezingC     Celsius = 0
-	BoilingC      Celsius = 100
+
+	// FreezingC represents value for the water freezing point.
+	FreezingC Celsius = 0
+
+	// BoilingC represents value for the water boiling point.
+	BoilingC Celsius = 100
 )
 
+// String returns Celsius degree with unit (°C).
 func (c Celsius) String() string {
 	return fmt.Sprintf("%g°C", c)
 }
 
+// String returns Fahrenheit degree with unit (°F).
 func (f Fahrenheit) String() string {
 	return fmt.Sprintf("%g°F", f)
 }
 
+// String returns Kelvin degree with unit (°K).
 func (k Kelvin) String() string {
 	return fmt.Sprintf("%gK", k)
 }
