d58eceef5512abfd51ebef2a7c467f4a92b958f0
ohta <yukiohta234@gmail.com>
2016-10-01 22:58:32 +0900

    add ch05 ex06

---
+++ ch05/ex06-surface/main.go
@@ -0,0 +1,61 @@
+// ex01-surface computes an SVG rendering of a 3-D surface function.
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320
+	cells         = 100
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
+func main() {
+	fmt.Printf("<svg xmlns ='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			if ax, ay, ok := corner(i+1, j); !ok {
+				continue
+			} else if bx, by, ok := corner(i, j); !ok {
+				continue
+			} else if cx, cy, ok := corner(i, j+1); !ok {
+				continue
+			} else if dx, dy, ok := corner(i+1, j+1); !ok {
+				continue
+			} else {
+				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy)
+			}
+		}
+	}
+	fmt.Println("</svg>")
+}
+
+func corner(i, j int) (sx, sy float64, ok bool) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	z := f(x, y)
+	if math.IsInf(z, 0) || math.IsNaN(z) {
+		return
+	}
+
+	sx = width/2 + (x-y)*cos30*xyscale
+	sy = height/2 + (x+y)*sin30*xyscale - z*zscale
+	ok = true
+	return
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+	return math.Sin(r) / r
+}
