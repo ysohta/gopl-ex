a514e426881ba141df96ab447d9ed6f4b3a22cb7
ohta <yukiohta234@gmail.com>
2016-10-19 19:36:23 +0900

    remove unused method

--- ch06/ex04-elems/intset.go
+++ ch06/ex04-elems/intset.go
@@ -1,10 +1,5 @@
 package intset
 
-import (
-	"bytes"
-	"fmt"
-)
-
 type IntSet struct {
 	words []uint64
 }
@@ -41,24 +36,4 @@ func (s *IntSet) Add(x int) {
 		s.words = append(s.words, 0)
 	}
 	s.words[word] |= 1 << bit
-}
-
-func (s *IntSet) String() string {
-	var buf bytes.Buffer
-	buf.WriteByte('{')
-	for i, word := range s.words {
-		if word == 0 {
-			continue
-		}
-		for j := 0; j < 64; j++ {
-			if word&(1<<uint(j)) != 0 {
-				if buf.Len() > len("{") {
-					buf.WriteByte(' ')
-				}
-				fmt.Fprintf(&buf, "%d", 64*i+j)
-			}
-		}
-	}
-	buf.WriteByte('}')
-	return buf.String()
 }
