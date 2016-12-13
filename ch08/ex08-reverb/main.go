e4ee0ea397877c7b375181f73c91e99729405c0b
ohta <yukiohta234@gmail.com>
2016-12-14 00:19:19 +0900

    add ch08 ex08

---
+++ ch08/ex08-reverb/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+var (
+	reset = make(chan struct{})
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+
+		// reset countdown
+		reset <- struct{}{}
+	}
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				log.Print(err) // e.g., connection aborted
+				continue
+			}
+
+			go handleConn(conn)
+		}
+	}()
+
+	tick := time.Tick(1 * time.Second)
+	countdown := 10
+	for countdown > 0 {
+		fmt.Println(countdown)
+		select {
+		case <-tick:
+			countdown--
+		case <-reset:
+			countdown = 10
+		}
+	}
+}
