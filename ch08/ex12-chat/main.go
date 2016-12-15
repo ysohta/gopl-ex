7eb369e19a57016074925112b110088c63872f31
ohta <yukiohta234@gmail.com>
2016-12-16 08:55:26 +0900

    add ch08 ex12

---
+++ ch08/ex12-chat/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+type client struct {
+	ch   chan<- string // an outgoing message channel
+	name string
+}
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string) // all incoming client messages
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages:
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli.ch <- msg
+			}
+
+		case cli := <-entering:
+			// online list
+			online := []string{}
+			for c := range clients {
+				online = append(online, c.name)
+			}
+
+			msgOnline := "no one is online"
+			if len(online) > 0 {
+				msgOnline = fmt.Sprintf("online list: %s", strings.Join(online, " "))
+			}
+			cli.ch <- msgOnline
+
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.ch)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // outgoing client messages
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	cli := client{ch, who}
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- cli
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+	// NOTE: ignoring potential errors from input.Err()
+
+	leaving <- cli
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
