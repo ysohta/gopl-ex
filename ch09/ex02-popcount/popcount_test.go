829d060cd9cf509109bbd3f1876042fc499333d2
ohta <yukiohta234@gmail.com>
2016-12-20 19:32:48 +0900

    fix to call testee

--- ch09/ex02-popcount/popcount_test.go
+++ ch09/ex02-popcount/popcount_test.go
@@ -1,61 +1,25 @@
-package popcount_test
+package popcount
 
 import (
 	"testing"
-
-	"gopl.io/ch2/popcount"
 )
 
-func BitCount(x uint64) int {
-	// Hacker's Delight, Figure 5-2.
-	x = x - ((x >> 1) & 0x5555555555555555)
-	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
-	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
-	x = x + (x >> 8)
-	x = x + (x >> 16)
-	x = x + (x >> 32)
-	return int(x & 0x7f)
-}
-
-func PopCountByClearing(x uint64) int {
-	n := 0
-	for x != 0 {
-		x = x & (x - 1) // clear rightmost non-zero bit
-		n++
+func TestPopcount(t *testing.T) {
+	tests := []struct {
+		n    uint64
+		want int
+	}{
+		{0x00, 0},
+		{0x01, 1},
+		{0x03, 2},
+		{0x10, 1},
+		{0xffffffffffffffff, 64},
 	}
-	return n
-}
 
-func PopCountByShifting(x uint64) int {
-	n := 0
-	for i := uint(0); i < 64; i++ {
-		if x&(1<<i) != 0 {
-			n++
+	for _, test := range tests {
+		actual := PopCount(test.n)
+		if actual != test.want {
+			t.Errorf("PopCount(%x) Expected:%d Actual:%d", test.n, test.want, actual)
 		}
-	}
-	return n
-}
-
-func BenchmarkPopCount(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		popcount.PopCount(0x1234567890ABCDEF)
-	}
-}
-
-func BenchmarkBitCount(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		BitCount(0x1234567890ABCDEF)
-	}
-}
-
-func BenchmarkPopCountByClearing(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		PopCountByClearing(0x1234567890ABCDEF)
-	}
-}
-
-func BenchmarkPopCountByShifting(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		PopCountByShifting(0x1234567890ABCDEF)
 	}
 }
